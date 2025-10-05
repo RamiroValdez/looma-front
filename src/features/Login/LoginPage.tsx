6c363d578d2842fd9e379dd5dbbceb2c88fedf38
RamiroValdez <rvaldez234@alumno.unlam.edu.ar>
2025-10-04 21:30:15 -0300

    Implementada comunicacón con backend en la vista MyWorks.tsx, además refactoricé las consultas y dejé un esqueleto de como deberían hacerse

--- src/features/Login/LoginPage.tsx
+++ src/features/Login/LoginPage.tsx
@@ -80,7 +80,7 @@ export const LoginPage = () => {
                     <button
                         type="submit"
                         disabled={loading}
-                        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:bg-gray-400 disabled:cursor-not-allowed transition duration-150 ease-in-out"
+                        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#5C17A6] hover:bg-[#521594] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#761ED4] disabled:bg-gray-400 disabled:cursor-not-allowed transition duration-150 ease-in-out"
                     >
                         {loading ? 'Cargando...' : 'Login'}
                     </button>
