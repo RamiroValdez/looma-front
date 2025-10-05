866b40c40430927c7891eae315e909b261eda541
MayraFazzari <mayrafazzariclerici.01@gmail.com>
2025-10-04 21:40:59 -0300

    header -v2

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -1,4 +1,19 @@
-export async function getCategorias() {
-  const res = await fetch("/data/categorias.json");
-  return res.json();
+import { type CategoryDTO } from "../dtos/category.dto";
+
+export async function getCategorias(): Promise<CategoryDTO[]> {
+  try {
+    const response = await fetch("/data/categorias.json"); 
+
+    if (!response.ok) {
+      throw new Error(`Error al obtener categorías: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+
+    return data as CategoryDTO[];
+  } catch (error) {
+    console.error("Error al obtener las categorías:", error);
+    throw error;
+  }
 }
