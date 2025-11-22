38f99b5e3447bd7835e46098551af45f5319a608
AlanCarp91218 <alandominguezcarp1@gmail.com>
2025-11-22 01:23:20 -0300

    responsive v1

--- src/app/components/BackButton.tsx
+++ src/app/components/BackButton.tsx
@@ -15,8 +15,7 @@ const BackButton = ({ to }: BackButtonProps) => {
   return (
     <button
       onClick={handleClick}
-      className="flex items-center gap-2 bg-[#5c17a6] text-white backdrop-blur-sm px-4 py-1.5 rounded-full hover:text-white hover:border-[#172fa6] hover:shadow-md transition-all duration-200 group mb-3 cursor-pointer"
-    >
+    className="flex items-center gap-2 bg-[#5c17a6] py-1.5 rounded-full text-white backdrop-blur-sm hover:text-white hover:border-[#172fa6] hover:shadow-md transition-all duration-200 group mb-3 cursor-pointer px-2 sm:px-4"    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-4 w-4 transform group-hover:-translate-x-0.5 transition-transform duration-200"
@@ -24,9 +23,9 @@ const BackButton = ({ to }: BackButtonProps) => {
         viewBox="0 0 24 24"
         stroke="currentColor"
       >
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M15 19l-7-7 7-7" />
       </svg>
-      <span className="text-sm font-medium">Volver</span>
+      <span className="text-sm font-medium hidden sm:block">Volver</span>
     </button>
   );
 };
