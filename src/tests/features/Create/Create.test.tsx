fd54f129dd38b9b7d3c94d82b5109d1d2de8c77b
AlanCarp91218 <alandominguezcarp1@gmail.com>
2025-11-25 23:13:11 -0300

    refactor create work

--- src/tests/features/Create/Create.test.tsx
+++ src/tests/features/Create/Create.test.tsx
@@ -2,7 +2,7 @@ import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
-import Create from '../../../app/features/Work/Create';
+import Create from '../../../app/features/Work/Create/Create';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const mockNavigate = vi.fn();
